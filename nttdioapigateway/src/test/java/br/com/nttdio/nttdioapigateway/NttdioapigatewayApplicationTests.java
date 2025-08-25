b20c12bc21d20b8754ebf44f4fd1bba616f7c045
Dimitri Ferraz <dvsferraz@gmail.com>
2025-08-24 23:14:20 -0300

    Adicionado o código do apigateway

---
+++ nttdioapigateway/src/test/java/br/com/nttdio/nttdioapigateway/NttdioapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nttdio.nttdioapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttdioapigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
